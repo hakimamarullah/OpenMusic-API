ecc49543a322f8a4fb9e97302086bfa0f9b0205d
Hakim Amarullah <hakimamarullah@gmail.com>
2022-09-11 01:56:33 +0700

    Feature endpoint for Song

---
+++ src/api/songs/handler.js
@@ -0,0 +1,125 @@
+const ClientError = require('../../exceptions/ClientError');
+
+/* eslint-disable no-underscore-dangle */
+class SongHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postSongHandler = this.postSongHandler.bind(this);
+    this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
+    this.deleteSongByIdHandler = this.deleteSongByIdHandler.bind(this);
+    this.getAllSongsHandler = this.getAllSongsHandler.bind(this);
+  }
+
+  async postSongHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const {
+        title, year, genre, performer, duration, albumId,
+      } = request.payload;
+
+      const songId = await this._service.addSong({
+        title, year, genre, performer, duration, albumId,
+      });
+
+      return h.response({
+        status: 'success',
+        data: {
+          songId,
+        },
+      }).code(201);
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+      console.log(error);
+      return h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      }).code(500);
+    }
+  }
+
+  async getAllSongsHandler() {
+    const result = await this._service.getAllSongs();
+    return {
+      status: 'success',
+      data: {
+        songs: result,
+      },
+    };
+  }
+
+  async getSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+
+      const result = await this._service.getSongById(id);
+
+      return h.response({
+        status: 'success',
+        data: {
+          song: result,
+        },
+      }).code(200);
+    } catch (error) {
+      return h.response({
+        status: 'fail',
+        message: error.message,
+      }).code(404);
+    }
+  }
+
+  async putSongByIdHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const {
+        title, year, genre, performer, duration, albumId,
+      } = request.payload;
+      const { id } = request.params;
+
+      const result = await this._service.putSongById(id, {
+        title, year, genre, performer, duration, albumId,
+      });
+
+      return h.response({
+        status: 'success',
+        message: `Lagu ${result} berhasil diubah`,
+      }).code(200);
+    } catch (error) {
+      if (error instanceof ClientError) {
+        return h.response({
+          status: 'fail',
+          message: error.message,
+        }).code(error.statusCode);
+      }
+      return h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      }).code(500);
+    }
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      await this._service.deleteSongById(id);
+      return h.response({
+        status: 'success',
+        message: `Lagu ${id} berhasil dihapus`,
+      }).code(200);
+    } catch (error) {
+      return h.response({
+        status: 'fail',
+        message: error.message,
+      }).code(404);
+    }
+  }
+}
+
+module.exports = SongHandler;
---
+++ src/services/SongService.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-underscore-dangle */
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../exceptions/InvariantError');
+const NotFoundError = require('../exceptions/NotFoundError');
+
+class SongService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addSong({
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const id = `song-${nanoid(16)}`;
+    const query = {
+      text: `INSERT INTO song
+      VALUES($1,$2,$3, $4, $5, $6, $7) RETURNING id`,
+      values: [id, title, year, genre, performer, duration, albumId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongById(id) {
+    const query = {
+      text: 'SELECT * FROM song where id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu tidak ditemukan');
+    }
+
+    return result.rows[0];
+  }
+
+  async getAllSongs() {
+    const result = await this._pool.query('SELECT * FROM song');
+    return result.rows;
+  }
+
+  async putSongById(id, {
+    title, year, genre, performer, duration, albumId,
+  }) {
+    const query = {
+      text: `UPDATE song 
+      SET title = $2, year = $3, genre = $4, performer = $5, duration = $6, albumId = $7 where id = $1 RETURNING id`,
+      values: [id, title, year, genre, performer, duration, albumId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal update lagu. Id tidak ditemukan');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async deleteSongById(id) {
+    const query = {
+      text: 'DELETE FROM song where id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new NotFoundError('Gagal menghapus lagu. Id tidak ditemukan');
+    }
+  }
+}
+module.exports = SongService;
