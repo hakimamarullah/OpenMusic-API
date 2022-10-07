66e9d957fe5886885681bb56b0a7e61ac2304b03
Capt. Hakim Amarullah <61151625+hakimamarullah@users.noreply.github.com>
2022-10-07 12:58:18 +0700

    Authentication implemented (#8)

---
+++ src/api/users/routes.js
@@ -0,0 +1,20 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/users',
+    handler: handler.postUserHandler,
+  },
+  {
+    method: 'GET',
+    path: '/users/{id}',
+    handler: handler.getUserByIdHandler,
+  },
+  {
+    method: 'GET',
+    path: '/users',
+    handler: handler.getUsersByUsernameHandler,
+  },
+
+];
+
+module.exports = routes;
